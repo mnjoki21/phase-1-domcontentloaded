d02838eed84656151af689c098a099928fa8b821
mitchelle <mitchellenjoki7@gmail.com>
2022-06-13 23:12:14 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Your code goes here
+
+function domContentLoaded() { 
+
+    
+    document.addEventListener("DOMContentLoaded", function(event) {
+      updateDOM()
+    }); 
+  } 
+  
+
+  document.addEventListener( "DOMContentLoaded", function () {
+    updateDOM()
+  } );
+  
+  
+  function updateDOM() {
+    document.getElementById("text").innerHTML = "This is really cool!";
+    document.getElementById( "text" )
+      .innerHTML = "This is really cool!";
+  }
+  
